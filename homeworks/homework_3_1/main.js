d4a907f05241c682bb91d4bd65830fd7211d56cc
marynaadudnyk <134285801+marynaadudnyk@users.noreply.github.com>
2025-02-09 13:57:32 +0200

    add Homework 3.1

---
+++ homeworks/homework_3_1/main.js
@@ -0,0 +1,26 @@
+ // Number
+console.log("Number:", typeof 10);
+
+// String
+console.log("String:", typeof "10");
+
+// Undefined
+console.log("Undefined:", typeof undefined);
+
+// Boolean
+console.log("Boolean:", typeof false);
+
+// Bigint
+console.log("Bigint:", typeof 9007199254740991n);
+
+// Null
+console.log("null:", typeof null);
+
+// Array
+console.log("array:", typeof [10, 9, 8]);
+
+ // Object
+console.log("Object:", typeof { color: "red", size: "big" });
+
+// Function
+console.log("Function:", typeof function () {});
