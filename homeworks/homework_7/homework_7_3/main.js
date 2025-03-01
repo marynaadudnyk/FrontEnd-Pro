fd7e4b76740a51a48192ccf95173eb60031f3726
marynaadudnyk <134285801+marynaadudnyk@users.noreply.github.com>
2025-03-01 19:46:23 +0200

    add homework_7_3

--- homeworks/homework_7/homework_7_3/main.js
+++ homeworks/homework_7/homework_7_3/main.js
@@ -3,3 +3,17 @@
 const array = [1, 3, 4, 6, 2, 5, 7];
 removeElement(array, 4);
 console.log(array); */
+
+const array = [1, 3, 4, 6, 2, 5, 7];
+
+function removeElement(array, item) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === item) {
+            array.splice(i, 1);
+            break;
+        }
+    }
+}
+
+removeElement(array, 4);
+console.log(array);
