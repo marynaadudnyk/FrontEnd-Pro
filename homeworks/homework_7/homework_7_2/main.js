a00231a9e2078c44f2d40e67fd43c4002671b1a2
marynaadudnyk <134285801+marynaadudnyk@users.noreply.github.com>
2025-03-02 20:40:09 +0200

    add commit

--- homeworks/homework_7/homework_7_2/main.js
+++ homeworks/homework_7/homework_7_2/main.js
@@ -1,6 +1,6 @@
 /* Дано масив з елементами різних типів.Створити функцію яка вираховує середнє арифметичне лише числових елементів даного масиву. */
 
-let arr = [300, true, 'hi', -10, 36, 'hello', 389, null, 39];
+let arr = [300, '578', true, 'hi', -10, 36, 'hello', 389, null, 39, '40'];
 
 function functionArr(arr) {
     let result = 0;
@@ -8,7 +8,7 @@ function functionArr(arr) {
     let total;
 
     for (let i = 0; i < arr.length; i++) {
-        if (typeof arr[i] === 'number' && !isNaN(arr[i])) {
+        if (typeof arr[i] === 'number') {
             result = result + arr[i];
             count++;
         }
