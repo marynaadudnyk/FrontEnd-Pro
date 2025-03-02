96a9f7ea8eab55d63206160d5d8adbd956acbeaf
marynaadudnyk <134285801+marynaadudnyk@users.noreply.github.com>
2025-03-02 20:41:06 +0200

    add

--- homeworks/homework_7/homework_7_2/main.js
+++ homeworks/homework_7/homework_7_2/main.js
@@ -5,7 +5,6 @@ let arr = [300, '578', true, 'hi', -10, 36, 'hello', 389, null, 39, '40'];
 function functionArr(arr) {
     let result = 0;
     let count = 0;
-    let total;
 
     for (let i = 0; i < arr.length; i++) {
         if (typeof arr[i] === 'number') {
