ac3168fa3baa606fb41b32a9aec295546519e032
marynaadudnyk <134285801+marynaadudnyk@users.noreply.github.com>
2025-03-03 16:06:07 +0200

    add homework_8_3

--- homeworks/homework_8/homework_8_3/main.js
+++ homeworks/homework_8/homework_8_3/main.js
@@ -1,1 +1,20 @@
 /* Цикл на кожній ітерації пропонує через prompt ввести число більше 100(але максимум 10 ітерацій циклу).Якщо відвідувач ввів число менше ста – попросити ввести ще раз, і таке інше.Якщо користувач вводить більше ста, текст або цикл закінчує всі ітерації, то функція виводить в консоль останній введення користувача і завершує функцію. */
+
+function arr() {
+    let min = 100;
+    let maxCount = 10;
+    let number;
+
+    for (let i = 0; i < maxCount; i++) {
+        number = prompt(`Enter a number greater than 100:`);
+
+        if (parseInt(number) > min || isNaN(number)) {
+            break;
+        }
+    }
+
+    console.log(number);
+    return number;
+}
+
+arr();
